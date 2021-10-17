173c8f3ddecdee1079937dc87dfc0284c63055e1
Santiago <santisz7897@gmail.com>
2021-10-17 15:47:33 +0300

    add the cov function

---  anomaly_detection_util.cpp
+++  anomaly_detection_util.cpp
@@ -3,3 +3,23 @@
 //
 
 #include " anomaly_detection_util.h"
+
+float cov(float* x, float* y, int size){
+    float tempArr[size];
+    float xAverage = 0, yAverage = 0, xyAverage = 0;
+    for(int i = 0; i < size; i++){
+        // for x*y
+        tempArr[i] = x[i] * y[i];
+        // adding the sum to x and y
+        xAverage += x[i];
+        yAverage += y[i];
+        xyAverage += tempArr[i];
+
+    }
+    xAverage /= size;
+    yAverage /= size;
+    xyAverage /= size;
+    // E(xy) - E(x)E(y)
+    return xyAverage - xAverage * yAverage;
+    
+}
