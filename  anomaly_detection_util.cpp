3c65d334fd19c7c3c970d4f9145a76f46f05e480
ido <idoddii@gmail.com>
2021-10-17 15:44:48 +0300

    added var function

---  anomaly_detection_util.cpp
+++  anomaly_detection_util.cpp
@@ -1,5 +1,19 @@
-//
-// Created by ido on 17/10/2021.
-//
+
 
 #include " anomaly_detection_util.h"
+
+// returns the variance of X and Y
+float var(float* x, int size) {
+    float z = 0;
+    // the mio of var(x)
+    float mio = 0;
+    for (int i = 0; i < size; i++) {
+        mio += x[i];
+        z += x[i] * x[i];
+    }
+    mio /= (float)size;
+    mio *= mio;
+    z /= (float)size;
+    z -= mio;
+    return z;
+}
