ab78a8dc10730448a23fb78657f15baadb085ef4
ido <idoddii@gmail.com>
2021-11-02 11:21:14 +0200

    s

--- AnomalyDetector.h
+++ AnomalyDetector.h
@@ -1,7 +1,28 @@
 #ifndef IDOSANTI_ANOMALYDETECTOR_H
 #define IDOSANTI_ANOMALYDETECTOR_H
+#include <vector>
+#include <map>
+#include <cstring>
+#include <iostream>
+#include <fstream>
 
-class TimeSeries{ /*...*/};
+class TimeSeries{
+    std::map<std::string, std::vector<float>> table;
+
+public:
+    TimeSeries()= default;
+    void readFromFile(std::ifstream file) {
+        while(file.good()) {
+            std::string key;
+            getline(file, line, ',');
+            table.insert(line, std::vector<float>());
+        }
+    }
+
+    std::vector<float> GetFeature(std::string& featureName) const {
+        return table.extract(featureName);
+    }
+};
 class AnomalyReport{
 public:
     const string description;
