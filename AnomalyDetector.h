08b0b5b3aae586a681488a8d9e6f890fb5801b36
Santiago <santisz7897@gmail.com>
2021-11-05 16:10:45 +0200

    added the readFromFile method

--- AnomalyDetector.h
+++ AnomalyDetector.h
@@ -12,10 +12,26 @@ class TimeSeries{
 public:
     TimeSeries()= default;
     void readFromFile(std::ifstream file) {
-        while(file.good()) {
-            std::string key;
-            getline(file, line, ',');
-            table.insert(line, std::vector<float>());
+        std::vector<std::string> keys;
+        bool isFirstLine = true;
+
+        while(file.good()){
+            std::string line;
+            getline(file, line, '\n');
+            std::istringstream newLine(line);
+            std::string word;
+            int i = 0;
+            while(getline(newLine, word, ',')){
+                if (isFirstLine) {
+                    std::vector<float> vec;
+                    keys.push_back(word);
+                    table.insert(std::make_pair(word, vec));
+                } else {
+                    table[keys[i]].push_back(std::stof(word));
+                }
+                i++;
+            }
+            isFirstLine = false;
         }
     }
 
