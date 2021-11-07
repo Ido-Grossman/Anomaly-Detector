96ec020a7db14571b1802052d1f7d75be61a1805
ido <idoddii@gmail.com>
2021-11-07 23:44:06 +0200

    Finished detect

--- AnomalyDetector.h
+++ AnomalyDetector.h
@@ -4,21 +4,19 @@
 #include <map>
 #include <cstring>
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include "timeseries.h"
 
 class AnomalyReport{
 public:
     const std::string description;
     const long timeStep;
-    AnomalyReport(string description, long timeStep) :
+    AnomalyReport(std::string description, long timeStep) :
             description(description),timeStep(timeStep){}
 };
 class TimeSeriesAnomalyDetector {
 public:
     virtual void learnNormal(const TimeSeries& ts)=0;
-    virtual vector<AnomalyReport> detect(const TimeSeries& ts)=0;
+    virtual std::vector<AnomalyReport> detect(const TimeSeries& ts)=0;
     virtual ~TimeSeriesAnomalyDetector(){}
 };
 
--- timeseries.h
+++ timeseries.h
@@ -1,8 +1,11 @@
 
 #ifndef TIMESERIES_H_
 #define TIMESERIES_H_
+#include <map>
+#include <vector>
+#include <fstream>
+#include <sstream>
 
-#include "AnomalyDetector.h"
 class TimeSeries{
     // table we create from the csv file
     std::map<std::string, std::vector<float>> table;
