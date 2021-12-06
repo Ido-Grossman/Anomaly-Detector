42c96c206dbb1e58226a3593825d081261136f66
ido <idoddii@gmail.com>
2021-12-06 14:55:38 +0200

    Create circle

--- minCIrcle.cpp
+++ minCIrcle.cpp
@@ -1,6 +1,6 @@
+// ID: 324603422 (Santiago Szterenberg) && 208985424 (Ido Grossman).
 #include "anomaly_detection_util.h"
 
-// ID: 324603422 (Santiago Szterenberg) && 208985424 (Ido Grossman).
 class Circle {
 private:
     Point center;
