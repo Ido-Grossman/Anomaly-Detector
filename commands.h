89133c6df4c0546fe232957df56b7ceb2422bbe9
ido <idoddii@gmail.com>
2021-12-16 12:24:56 +0200

    Final2

--- commands.h
+++ commands.h
@@ -58,6 +58,10 @@ public:
 class Upload : public Command {
 public:
     Upload(DefaultIO* dio): Command(dio, "upload a time series csv file"){}
+    /*
+    * First requests the user to upload is train CSV, and then upload his test CSV, and it reads the files and
+    * stores the train in anomalyTrain.csv and the test in anomalyTest.csv.
+    */
     void execute(struct Ts* ts) override {
         dio->write("Please upload your local train CSV file.\n");
         dio->readFiles("anomalyTrain.csv");
@@ -72,12 +76,17 @@ class Thresh : public Command {
     float userThreshold = 0;
 public:
     Thresh(DefaultIO* dio): Command(dio, "algorithm settings"){}
+    /*
+     * Prints the user the current threshold and gives him the option to change the threshold to his desired
+     * threshold.
+     */
     void execute(struct Ts* ts) override {
         dio->write("The current correlation threshold is ");
         dio->write(ts->threshold);
         dio->write("\n");
         dio->write("Type a new threshold\n");
         dio->read(&userThreshold);
+        // Makes sure the user inputted threshold is valid and between 0 and 1.
         while (userThreshold > 1 && userThreshold < 0) {
             dio->write("please choose a value between 0 and 1.\n");
             dio->read(&userThreshold);
@@ -89,6 +98,10 @@ public:
 class Detect : public Command {
 public:
     Detect(DefaultIO* dio):Command(dio, "detect anomalies"){}
+    /*
+     * By the train and test files given in 1 the hybrid anomaly detector detects all the anomalies in the csv file
+     * the user uploaded.
+     */
     void execute(struct Ts* ts) override {
         TimeSeries train("anomalyTrain.csv");
         TimeSeries test("anomalyTest.csv");
@@ -104,6 +117,9 @@ public:
 class Results : public Command {
 public:
     Results(DefaultIO* dio):Command(dio, "display results"){}
+    /*
+     * Displays the detected anomalies in the user train and test files.
+     */
     void execute(struct Ts* ts) override {
         for(const AnomalyReport& report : ts->reports) {
             dio->write((float) report.timeStep);
@@ -116,7 +132,10 @@ public:
 
 class Analyze : public Command {
 private:
-
+    /*
+     * Given the original reports of the anomaly detector it merges all the reports that their description matches and
+     * their time step is +1 after the previous time step.
+     */
     void mergeReports(struct Ts* ts, vector<Anomaly>& originReports) {
         ulong size = ts->reports.size();
         for (int i = 0; i < size; i++) {
@@ -124,6 +143,10 @@ private:
             anomaly.startTime = ts->reports[i].timeStep;
             anomaly.endTime = ts->reports[i].timeStep;
             anomaly.description = ts->reports[i].description;
+            /*
+             * Goes through the time steps from the detector and checks if they are right one after another and if
+             * they are it merges them.
+             */
             for (int j = i + 1; j < size; j++) {
                 if (ts->reports[j].timeStep == anomaly.endTime + 1 &&
                     ts->reports[j].description == anomaly.description) {
@@ -142,12 +165,18 @@ private:
     }
 public:
     Analyze(DefaultIO* dio): Command(dio, "upload anomalies and analyze results"){}
+    /*
+     * Takes a reports file from the user and checks which of the reports the detector detected are true and which
+     * are false.
+     */
     void execute(struct Ts* ts) override {
         vector<Anomaly> userReports;
         vector<Anomaly> originReports;
         dio->write("Please upload your local anomalies file.\n");
+        // Merges all the original reports.
         mergeReports(ts, originReports);
         string line = "";
+        // Reads the user file and pushes all his reports into a vector.
         while ((line = dio->read()) != "done") {
             ulong where = line.find(',');
             Anomaly anomaly;
@@ -164,6 +193,11 @@ public:
         for (int i = 0; i < p; i++, foundOne = false) {
             n -= (userReports[i].endTime - userReports[i].startTime) + 1;
         }
+        /*
+         * For each original reports the detector found it checks if there is a report the user uploaded that
+         * is in the same time with the original report, if there is it marks the report as positive, else it marks
+         * it as false.
+         */
         for (Anomaly& originReport : originReports) {
             for (Anomaly& userReport : userReports) {
                 if ((originReport.startTime >= userReport.startTime &&
@@ -181,6 +215,7 @@ public:
                 fp++;
             foundOne = false;
         }
+        // Calculates the true positive rate and false positive rate up to 3 digits after the dot.
         float trueRate = floorf(((float) tp / (float) p) * 1000) / 1000;
         float falseRate = floorf(((float) fp / (float) n) * 1000) / 1000;
         dio->write("True Positive Rate: ");
@@ -199,7 +234,6 @@ public:
     }
 };
 
-// implement here your command classes
 
 
 
